ea2ba82c9662425fc3a8732ed2ab823d0a8a867e
bmpenuelas <bmpenuelas@gmail.com>
2018-04-18 21:47:33 +0200

    Long description.

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-
 # To setup this package, run "pip install -e ."
 
 from setuptools import setup
 
-def readme():
-    with open('./README.md') as f:
-        return f.read()
+
 
 setup(name='dictiorm',
-    version='0.1.post2',
+    version='0.1.post3',
     description='A tiny MongoDB ORM that takes zero time to setup because docs become simple dicts.',
-    long_description=readme(),
+    long_description='Use your database documents like a simple dictionary variable. Even in functions that expect a dict!',
     classifiers=[
       'Development Status :: 3 - Alpha',
       'Programming Language :: Python :: 3',
     ],
     keywords=['ORM', 'MongoDB', 'dictionary', 'dictiorm'],
     url='https://github.com/bmpenuelas/dictiORM',
-    download_url = 'https://github.com/bmpenuelas/dictiORM/archive/0.1.post2.tar.gz',
+    download_url='https://github.com/bmpenuelas/dictiORM/archive/0.1.post3.tar.gz',
     author='Borja Penuelas',
     author_email='bmpenuelas@gmail.com',
     license='MIT',
