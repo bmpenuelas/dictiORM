bbdca11e53bff11c71d2625b0b0c5dfcc6b5f23d
Borja Penuelas <bmpenuelas@gmail.com>
2018-06-02 22:50:21 +0200

    Update package on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 
 
 setup(name='dictiorm',
-    version='0.1.post3',
+    version='0.2',
+    download_url='https://github.com/bmpenuelas/dictiORM/archive/0.2.tar.gz',
     description='A tiny MongoDB ORM that takes zero time to setup because docs become simple dicts.',
     long_description='Use your database documents like a simple dictionary variable. Even in functions that expect a dict!',
     classifiers=[
@@ -14,7 +15,6 @@ setup(name='dictiorm',
     ],
     keywords=['ORM', 'MongoDB', 'dictionary', 'dictiorm'],
     url='https://github.com/bmpenuelas/dictiORM',
-    download_url='https://github.com/bmpenuelas/dictiORM/archive/0.1.post3.tar.gz',
     author='Borja Penuelas',
     author_email='bmpenuelas@gmail.com',
     license='MIT',
